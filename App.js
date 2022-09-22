5a3b89365fb4bdbbf30e234b4d1f1d5d452c103c
Christian Gitter <c.gitter@heurich.de>
2022-09-22 20:05:44 +0200

    add signUp-Screen

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import LoginScreen from './screens/LoginScreen';
 import HomeScreen from './screens/HomeScreen';
+import SignupScreen from './screens/SignupScreen';
 
 const Stack = createNativeStackNavigator();
 
@@ -12,7 +13,9 @@ export default function App() {
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen options={{headerShown: false}} name="Login" component={LoginScreen} />
+        <Stack.Screen options={{headerShown: false}} name="Konto erstellen" component={SignupScreen} />
         <Stack.Screen name="Home" component={HomeScreen} />
+
       </Stack.Navigator>
     </NavigationContainer>
   );}
--- screens/SignupScreen.js
+++ screens/SignupScreen.js
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from 'react'
 import { auth } from '../firebase'
 import { useNavigation } from '@react-navigation/native'
 
-const LoginScreen = () => {
+const SignupScreen = () => {
 
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
@@ -20,23 +20,27 @@ const LoginScreen = () => {
      return unsubscribe
   }, [])
 
-  const handleSignUp = () => {
-    navigation.replace("Konto erstellen")
+  const handleBack = () => {
+    navigation.replace("Login")
   }
 
-const handleLogin = () => {
-   auth
-   .signInWithEmailAndPassword(email, password)
-   .then(userCredentials => {
-    const user = userCredentials.user;
-    console.log('Angemeldet mit:', user.email);
-  })
-  .catch(error => alert(error.message))
-}
+  const handleSignUp = () => {
+    auth
+    .createUserWithEmailAndPassword(email, password)
+    .then(userCredentials => {
+      const user = userCredentials.user;
+      console.log('Registriert mit:', user.email);
+    })
+    .catch(error => alert(error.message))
+  }
 
   return (
     <KeyboardAvoidingView style={styles.container} behavior="padding">
       <Image style={styles.logo} source={require('../assets/Firebase_Logo.png')}></Image>
+      <View style={styles.tipTextContainer}>
+      <Text style={styles.tipText}>Du bist neu hier? Kein Problem!</Text>
+      <Text style={styles.tipText}> Erstelle hier einfach dein kostenloses Konto</Text>
+      </View>
       <View style={styles.inputContainer}>
         <TextInput placeholder='E-Mail' 
          value={email} 
@@ -51,19 +55,18 @@ const handleLogin = () => {
         </TextInput>
       </View>
       <View style={styles.buttonContainer}>
-        <TouchableOpacity onPress={handleLogin} style={styles.buttonLogin}>
-          <Text style={styles.buttonText}>Login</Text>
+        <TouchableOpacity onPress={handleSignUp} style={styles.buttonSignup}>
+          <Text style={styles.buttonText}>Konto erstellen</Text>
         </TouchableOpacity>
-        <Text style={styles.accountText}>Du hast noch keinen Account?</Text>
-        <TouchableOpacity onPress={handleSignUp} style={styles.buttonRegister}>
-          <Text style={styles.buttonTextRegister}>Anmelden</Text>
+        <TouchableOpacity onPress={handleBack} style={styles.buttonBack}>
+          <Text style={styles.buttonTextBack}>zurück zum Login</Text>
         </TouchableOpacity>
       </View>
     </KeyboardAvoidingView>
   )
 }
 
-export default LoginScreen
+export default SignupScreen
 
 const styles = StyleSheet.create({
   container: {
@@ -90,7 +93,7 @@ const styles = StyleSheet.create({
     borderRadius:10,
 
   },
-  buttonLogin:{
+  buttonSignup:{
    width:'100%',
    backgroundColor:'lightgreen',
    borderRadius:10,
@@ -100,34 +103,35 @@ const styles = StyleSheet.create({
    alignItems:'center',
    justifyContent:'center',
   },
-  buttonRegister:{
-    width:'100%',
-    backgroundColor:'#FFC0B3',
-    borderRadius:10,
-    padding:12,
-    borderColor:'tomato',
-    borderWidth:1,
-    alignItems:'center',
-    justifyContent:'center',
-    
-
-  },
   buttonText: {
     color:'darkgreen'
-  },
-  buttonTextRegister: {
-    color:'#811900'
-  },
-  accountText:{
-    marginTop:20,
-    marginBottom:5,
-    color:'darkgrey',
-    fontSize:12,
   },
   logo:{
     width:'50%',
     height:60,
     resizeMode:'stretch',
     marginBottom:30
+  },
+  tipText:{
+    color: 'darkgrey',
+  },
+  tipTextContainer: {
+    marginBottom: 15,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  buttonBack: {
+    width:'100%',
+   backgroundColor:'#FFC0B3',
+   borderRadius:10,
+   padding:12,
+   borderColor:'tomato',
+   borderWidth:1,
+   alignItems:'center',
+   justifyContent:'center',
+   marginTop: 10
+  },
+  buttonTextBack: {
+    color:'#811900'
   }
 })
